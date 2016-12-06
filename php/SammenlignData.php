6cccb663deea069ec65f614ef15011b246849dbf
Zeatz <Nicolaisteam2@gmail.com>
2016-12-06 13:44:37 +0100

    mega syg javascript

---
+++ php/SammenlignData.php
@@ -0,0 +1,15 @@
+
+
+
+
+<?php
+require_once '../vendor/autoload.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('../');
+$twig = new Twig_Environment($loader, array(
+'auto_reload' => true
+));
+$template = $twig->loadTemplate('SamData.html.twig');
+echo $template->render(array('results' => $temp));
+?>
